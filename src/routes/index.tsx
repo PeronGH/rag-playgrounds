1df69d0955641e90bab95e936e638de09ebd983f
peron <peron_ap@icloud.com>
2024-01-23 19:24:47 +0000

    chore: clean up project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
@@ -7,7 +6,6 @@ export default function Home() {
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
         Hello world!
       </h1>
-      <Counter />
       <p class="mt-8">
         Visit{" "}
         <a
@@ -24,7 +22,8 @@ export default function Home() {
         {" - "}
         <A href="/about" class="text-sky-600 hover:underline">
           About Page
-        </A>{" "}
+        </A>
+        {" "}
       </p>
     </main>
   );
